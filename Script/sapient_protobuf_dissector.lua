6ee98d9c34e0d512c21c6d70a7e202079726487e
Hellikandra <naevis.sharks@gmail.com>
2023-07-07 11:00:40 +0200

    Update sapient_protobuf_dissector.lua

--- Script/sapient_protobuf_dissector.lua
+++ Script/sapient_protobuf_dissector.lua
@@ -18,26 +18,36 @@ do
         local f_length = ProtoField.uint32(name .. ".length", "Sapient Message Length", base.DEC)
         proto.fields = { f_length }
 
+		print("-- Start proto.dissector -- for a Sapient Message in ")
         proto.dissector = function(tvb, pinfo, tree)
             local subtree = tree:add(proto, tvb())
 
             local header = 2 -- VarInt protobuf codage Base 128 !!
-            local offset = 2
+            local offset = 0
             local remaining_len = tvb:len()
-    
+			print("Sapient Message length + header", remaining_len)
+			
+			print("pinfo.fragmented = ", pinfo.fragmented)
+            print("pinfo.desegment_len = ", pinfo.desegment_len)
+            print("pinfo.desegment_offset = ", pinfo.desegment_offset)
+            print("subtree = ", subtree)
+	
             while remaining_len > 0 do
+				print("-- remaining_len > 0 --")
                 if remaining_len < header then -- header not enought
                     pinfo.desegment_offset = offset
                     pinfo.desegment_len = DESEGMENT_ONE_MORE_SEGMENT
+					print("-- Debug -- remaining_len < header : exit")
                     return -1
                 end
 
                 -- Get Sapient Message Size --
                 local msg_len = ByteArray.new(tvb:bytes(0, header):tohex())
+				print("test msg length from bytearray : ", msg_len)
                 -- convert from ByteArray to Number
                 local msb_msg_len = msg_len:get_index(1)
                 local lsb_msg_len = msg_len:get_index(0)
-                -- as explained on google protobuf Base 128
+                -- as explained on google protobuf Base 128 -- https://protobuf.dev/programming-guides/encoding/
                 -- 1. Grab MSB 7 -> 2 Bytes with only 7 bits
                 -- 2. concat MSB - LSB
                 -- 3. Get the new value
@@ -45,23 +55,39 @@ do
                 local msb_msg_len_bit = bit.lshift(bit.tobit(msb_msg_len), 8) -- convert to bit, shift msb to his byte position
 
                 msg_len = bit.rshift(bit.bor(msb_msg_len_bit, lsb_msg_len_bit), 1) -- concat bit through OR operator, shift to right by 1 to drop the MSB 7 from the MSB message part
+				
+                msg_len = remaining_len - header
+				print("-- Debug -- msg_len : ", msg_len)
 
                 if remaining_len - header < msg_len then
                     pinfo.desegment_offset = offset
                     pinfo.desegment_len = msg_len - (remaining_len - header)
+					print("-- Debug -- remaining_len - header < msg_len: exit")
                     return -1
                 end
                 
                 subtree:add(f_length, msg_len)
-
+                print(subtree)
                 if msgtype ~= nil then
                     pinfo.private["pb_msg_type"] = "message," .. msgtype
                 end
+                print("Dissector.call :", Dissector.call)
+                print("protobuf_dissector :", protobuf_dissector)
+                print("tvb(offset, msg_len):tvb() :", tvb(offset, msg_len):tvb())
+                print("offset :", offset)
+                print("msg_len :", msg_len)
+                print("pinfo. : ", pinfo)
+                print("pinfo : ", pinfo)
+                print("subtree :", subtree)
+                
+				local success, error_message = pcall(Dissector.call, protobuf_dissector, tvb(offset, msg_len):tvb(), pinfo, subtree)
+                if  not success then
+					print("Error : " .. error_message)
+				end
 
-                pcall(Dissector.call, protobuf_dissector, tvb(offset, msg_len):tvb(), pinfo, subtree)
-
-                offset = offset + header + msg_len
-                remaining_len = remaining_len - header
+                print("offset : ", offset, "remaining_len : ", remaining_len)
+                remaining_len = remaining_len - msg_len
+                print("offset : ", offset, "remaining_len : ", remaining_len)
 
             end -- endof while remaining > 0 do
             pinfo.columns.protocol:set(name)
@@ -72,6 +98,6 @@ do
     end -- endof function create_sapient_protobuf_dissector
 
     -- add more protobuf dissectors with message types
-    create_sapient_protobuf_dissector("SapientMessages", "SapientMessage Sapient", "sapient.SapientMessage")
+    create_sapient_protobuf_dissector("SapientMessages", "SapientMessage Sapient", "sapient_msg.SapientMessage")
 
 end -- endof do
