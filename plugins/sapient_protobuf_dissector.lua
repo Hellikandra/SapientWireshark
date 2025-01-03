28a8eb07a47502b595f9daf4151e83811ef77bd0
Jean Vnh <naevis.sharks@gmail.com>
2025-01-03 17:32:56 +0100

    publish branch
    Publish the branch with modification.

--- plugins/sapient_protobuf_dissector.lua
+++ plugins/sapient_protobuf_dissector.lua
@@ -21,11 +21,11 @@ do
     }
     -- The TCP dissector will
     local function create_sapient_protobuf_dissector(name, desc, msgtype)
-        local proto = Proto(name, desc)
+        local proto = Proto(name, desc .. " v2")
         local f_length = ProtoField.uint32(name .. ".length", "Sapient Message Length", base.DEC)
         proto.fields = { f_length }
 
-        print("-- Debug -- Start proto.dissector -- for a Sapient Message in ")
+        -- print("-- Debug -- Start proto.dissector -- for a Sapient Message in ")
 
         proto.dissector = function(tvb, pinfo, tree)
             local subtree = tree:add(proto, tvb())
@@ -35,14 +35,14 @@ do
             local offset = 0
             local remaining_len = tvb:len()
 
-			print("-- Debug -- Sapient Message length + header = ", remaining_len)
-			print("-- Debug -- pinfo.fragmented = ", pinfo.fragmented)
-            print("-- Debug -- pinfo.desegment_len = ", pinfo.desegment_len)
-            print("-- Debug -- pinfo.desegment_offset = ", pinfo.desegment_offset)
+			-- print("-- Debug -- Sapient Message length + header = ", remaining_len)
+			-- print("-- Debug -- pinfo.fragmented = ", pinfo.fragmented)
+            -- print("-- Debug -- pinfo.desegment_len = ", pinfo.desegment_len)
+            -- print("-- Debug -- pinfo.desegment_offset = ", pinfo.desegment_offset)
             
             -- Check if we are handling a partial message from a previous segment
             if sapient_state.is_partial_message then
-                print("Handling a continuation of partial message.")
+                -- print("Handling a continuation of partial message.")
 
                 local msg_len = sapient_state.remaining_msg_len
                 
@@ -51,7 +51,7 @@ do
                     pinfo.desegment_offset = offset
                     pinfo.desegment_len = msg_len - remaining_len
                     
-                    print("-- Debug -- Partial message: remaining_len < msg_len, requesting more data")
+                    -- print("-- Debug -- Partial message: remaining_len < msg_len, requesting more data")
                     
                     return -1
                 end
@@ -61,8 +61,12 @@ do
 
                 -- Call the protobuf dissector to hanqdle the message
                 local success, error_message = pcall(Dissector.call, protobuf_dissector, tvb(offset, msg_len):tvb(), pinfo, subtree)
-                if not success then
-                    print("-- Error --" .. error_message)
+                if  not success then
+					print("Error : " .. error_message)
+                    pinfo.private["decoded_successfully"] = false
+				else
+                    -- Set the flag indicating successful decoding
+                    pinfo.private["decoded_successfully"] = true
                 end
 
                 -- Reset state ater handling the message
@@ -71,19 +75,19 @@ do
 
                 offset = offset + msg_len
                 remaining_len = remaining_len - msg_len
-                print("-- Debug -- offset: ", offset, "remaining_len: ", remaining_len)
+                -- print("-- Debug -- offset: ", offset, "remaining_len: ", remaining_len)
             end
 
             -- Continue with new message (starting with a header)
             while remaining_len > 0 do
-				print("-- Debug -- remaining_len > 0 --")
+				-- print("-- Debug -- remaining_len > 0 --")
 
                 -- Check if we have enough bytes for the header
                 if remaining_len < header then
                     pinfo.desegment_offset = offset
                     pinfo.desegment_len = DESEGMENT_ONE_MORE_SEGMENT
 					
-                    print("-- Debug -- remaining_len < header : exit")
+                    -- print("-- Debug -- remaining_len < header : exit")
                     
                     return -1
                 end
@@ -91,7 +95,7 @@ do
                 -- Read the message length (4 bytes; little-endian)
                 local msg_len = tvb(offset, header):le_uint()
                 sapient_state.msg_len = msg_len -- Store the message length (excluding the header)
-                print("-- Debug -- msg_len (little-endian):", msg_len)
+                -- print("-- Debug -- msg_len (little-endian):", msg_len)
 
                 -- Check if there is enough data remaining to handle the message
                 if remaining_len - header < msg_len then
@@ -102,7 +106,7 @@ do
                     pinfo.desegment_offset = offset + header
                     pinfo.desegment_len = msg_len - (header - remaining_len)
 					
-                    print("-- Debug -- remaining_len - header < msg_len: exit and store state")
+                    -- print("-- Debug -- remaining_len - header < msg_len: exit and store state")
                     
                     return -1
                 end
@@ -118,13 +122,17 @@ do
 				local success, error_message = pcall(Dissector.call, protobuf_dissector, tvb(offset + header, msg_len):tvb(), pinfo, subtree)
                 
                 if  not success then
-					print("Error : " .. error_message)
-				end
+					-- print("Error : " .. error_message)
+                    pinfo.private["decoded_successfully"] = false
+				else
+                    -- Set the flag indicating successful decoding
+                    pinfo.private["decoded_successfully"] = true
+                end
 
                 -- Update the remaing length of the offset for the next iteration
                 offset = offset + msg_len + header
                 remaining_len = remaining_len - (msg_len + header)
-                print("offset : ", offset, "remaining_len : ", remaining_len)
+                -- print("offset : ", offset, "remaining_len : ", remaining_len)
             end -- endof while remaining > 0 do
             
             -- Set the protocol name in the Wireshark columns
@@ -139,6 +147,6 @@ do
     end -- endof function create_sapient_protobuf_dissector
 
     -- Create the SapientMessages dissector with protobuf support
-    create_sapient_protobuf_dissector("SapientMessages", "SapientMessage Sapient", "sapient_msg.bsi_flex_335_v2_0.SapientMessage")
+    create_sapient_protobuf_dissector("SapientMessages_v2", "SapientMessage Sapient", "sapient_msg.bsi_flex_335_v2_0.SapientMessage")
 
 end -- endof do
